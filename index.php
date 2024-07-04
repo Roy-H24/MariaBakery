dfe3821b9714c649c4ffb04f0ba50719a68a8bf4
seiskaidl <seiskaidl@gmail.com>
2024-07-04 16:24:41 +0700

    Menambahkan video baru di halaman index..

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include 'header.php';
 
 <section>
     <video width="100%" height="800" autoplay muted>
-        <source src="video/video.mp4" type="video/mp4">
+        <source src="video/cakeVideo.mp4" type="video/mp4">
         <source src="video/video.ogg" type="video/ogg">
         Your browser does not support the video tag.
     </video>
